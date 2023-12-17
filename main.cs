26ce568ed41ede6d707d7b04d2f6454818342f32
Marko2155 <98662057+Marko2155@users.noreply.github.com>
2023-12-17 21:18:50 +0100

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,33 @@
+using System;
+
+using System.Collections.Generic;
+
+namespace Marko2155 {
+	public class LBLEd {
+		public static void Main(string[] args) {
+			List<string> fileContent = new List<string>();
+			bool saved = true;
+			while(true) {
+			string input = Console.ReadLine();
+
+			if (input.StartsWith("?w ") && input.Substring(3) != "") {
+				System.IO.File.WriteAllLines(input.Substring(3), fileContent.ToArray());
+				saved = true;
+				fileContent.Clear();
+			} else if (input == "?q") {
+				if (fileContent.Count == 0 && saved == true) {
+					Environment.Exit(1);
+				} else {
+					Console.WriteLine("[LBLEd] You have not saved the text you have written. Save it then quit.");
+				}
+			} else if (input == "?qa") {
+				Environment.Exit(0);
+			} else if (!input.StartsWith("?")) {
+				saved = false;
+				fileContent.Add(input);
+			}
+
+			}
+		}
+	}
+}
